f18be55138b1b0166e717a4f4a66ca97a5b9e7b3
speculatic <speculatic@gmail.com>
2023-06-14 05:19:28 -0700

    added 5 num summary and customization of barplot

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ if web_apps == "Exploratory Data Analysis":
       st.write(df)
 
     # Display relevant statistics about the dataset
-    st.write('# of Rows:', df.shape[0])
-    st.write('# of Columns:', df.shape[1])
-    st.write('# of Numerical:', len(df.select_dtypes(include=['int64', 'float64']).columns))
-    st.write('# of Categorical:', len(df.select_dtypes(include=['object']).columns))
-    st.write('# of Boolean:', len(df.select_dtypes(include=['bool']).columns))
+    st.write('Number of Rows:', df.shape[0])
+    st.write('Number of Columns:', df.shape[1])
+    st.write('Number of Numerical Variables:', len(df.select_dtypes(include=['int64', 'float64']).columns))
+    st.write('Number of Categorical Variables:', len(df.select_dtypes(include=['object']).columns))
+    st.write('Number of Boolean Variables:', len(df.select_dtypes(include=['bool']).columns))
 
     column_type = st.sidebar.selectbox('Select Data Type',
                                        ("Numerical", "Categorical", "Bool", "Date"))
@@ -34,45 +34,35 @@ if web_apps == "Exploratory Data Analysis":
       numerical_column = st.sidebar.selectbox(
           'Select a Column', df.select_dtypes(include=['int64', 'float64']).columns)
 
-      # histogram
+      # 5num Summary
+      st.write(df[numerical_column].describe())
+
+      # Distribution plot
       choose_color = st.color_picker('Pick a Color', "#69b3a2")
       choose_opacity = st.slider(
           'Color Opacity', min_value=0.0, max_value=1.0, step=0.05)
-
       hist_bins = st.slider('Number of bins', min_value=5,
                             max_value=150, value=30)
-      hist_title = st.text_input('Set Title', 'Histogram')
+      hist_title = st.text_input('Set Title', 'Distribution Plot')
       hist_xtitle = st.text_input('Set x-axis Title', numerical_column)
 
       fig, ax = plt.subplots()
-      ax.hist(df[numerical_column], bins=hist_bins,
-              edgecolor="black", color=choose_color, alpha=choose_opacity)
+      sns.histplot(df[numerical_column], bins=hist_bins, kde=True, color=choose_color, ax=ax)
       ax.set_title(hist_title)
       ax.set_xlabel(hist_xtitle)
-      ax.set_ylabel('Count')
-
+      ax.set_ylabel('Frequency')
       st.pyplot(fig)
-      filename = "plot.png"
-      fig.savefig(filename,dpi = 300)
-
-      # Display the download button
-      with open("plot.png", "rb") as file:
-        btn = st.download_button(
-            label="Download image",
-            data=file,
-            file_name="flower.png",
-            mime="image/png"
-        )
 
     elif column_type == "Categorical":
       categorical_column = st.sidebar.selectbox(
           'Select a Column', df.select_dtypes(include=['object']).columns)
 
-      # Display proportions of each category level
+      # Proportion
       st.write(df[categorical_column].value_counts(normalize=True))
 
       # Barplot
+      choose_color = st.color_picker('Pick a Color', "#69b3a2")
       fig, ax = plt.subplots()
-      sns.countplot(x=categorical_column, data=df, ax=ax)
+      sns.countplot(x=categorical_column, data=df, color=choose_color, ax=ax)
       plt.xticks(rotation=90)
       st.pyplot(fig)
